4f7b0eb31d33bc38f117f6d42aeed9b967313303
izeye <izeye@naver.com>
2014-12-19 11:45:29 +0900

    Add a startup script.

--- src/main/java/samples/springboot/admin/Application.java
+++ src/main/java/samples/springboot/admin/Application.java
@@ -2,6 +2,7 @@ package samples.springboot.admin;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.system.ApplicationPidListener;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +16,13 @@ import org.springframework.context.annotation.Configuration;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication application = new SpringApplication(Application.class);
+
+		// NOTE:
+		// ApplicationPidFileWriter is 1.2.0.RELEASE only.
+//		application.addListeners(new ApplicationPidFileWriter());
+		application.addListeners(new ApplicationPidListener());
+		application.run(args);
 	}
 
 }
