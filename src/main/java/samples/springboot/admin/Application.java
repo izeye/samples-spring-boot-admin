ed18c4b94b544d0bd9cc0f45d02d8ee31ffbd641
izeye <izeye@naver.com>
2015-09-12 13:40:08 +0900

    Upgrade Spring Boot Admin 1.2.3.

--- src/main/java/samples/springboot/admin/Application.java
+++ src/main/java/samples/springboot/admin/Application.java
@@ -2,26 +2,19 @@ package samples.springboot.admin;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.system.ApplicationPidListener;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by izeye on 14. 12. 18..
  */
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableAdminServer
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(Application.class);
-
-		// NOTE:
-		// ApplicationPidFileWriter is 1.2.0.RELEASE only.
-//		application.addListeners(new ApplicationPidFileWriter());
-		application.addListeners(new ApplicationPidListener());
+		application.addListeners(new ApplicationPidFileWriter());
 		application.run(args);
 	}
 
